661628b34b958801a94c3edead82a51164f93b33
a.kato <a.kato.micron.inc@gmail.com>
2024-09-30 15:07:57 +0900

    server.ts update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import express, { Request, Response } from 'express';
+// server.ts
+/*
+import * as http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World from TypeScript!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/');
+});
+*/
+
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', (req, res) => {
   res.send('Hello World from TypeScript!');
 });
 
